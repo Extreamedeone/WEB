44f91b258e15439f909cfed5c4be0cdfa902359a
Extreamedeone <kiokonzau921@gmail.com>
2025-11-25 16:34:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __path = process.cwd();
 const PORT = process.env.PORT || 10000;
 
 const qrRoute = require('./qr');
-const codeRoute = require('./pair.cjs');
+const codeRoute = require('./pair.mjs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
